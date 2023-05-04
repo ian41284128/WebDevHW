ea94e5bdba857318f4133d26de99b178ecbd16a8
ian41284128 <ian4128@nyu.edu>
2023-05-04 12:44:54 -0400

    room is done thank domo lol

--- final/main.js
+++ final/main.js
@@ -18,17 +18,19 @@ document.body.appendChild( renderer.domElement );
 
 const loader = new GLTFLoader();
 let room;
+let wall = 0;
 let roomRotation = 0;
+let rotateDir = 0;
 
 loader.load( 'room.glb', function ( gltf ) {
 
     scene.add(gltf.scene);
     room = gltf.scene;
-    /*const texture = new THREE.TextureLoader().load( "SmallLightmap.png" );
-    const material = new THREE.MeshPhongMaterial();
+    console.log(room);
+    /*const texture = new THREE.TextureLoader().load( "LightmapFull.png" );
+    const material = new THREE.MeshBasicMaterial({map: texture});
     gltf.scene.children.forEach(mesh => {
         mesh.material = material;
-        scene.add(mesh);
     });*/
 
 }, undefined, function ( error ) {
@@ -37,30 +39,56 @@ loader.load( 'room.glb', function ( gltf ) {
 
 } );
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-scene.add( directionalLight );
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
+// const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+// scene.add( directionalLight );
+// const light = new THREE.AmbientLight( 0x101010 ); // soft white light
+// scene.add( light );
 
 const clock = new THREE.Clock();
 
 ;(function animate() {
+    let dt = clock.getDelta();
     requestAnimationFrame( animate );
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
-    if(room){
-        room.rotation.y = Math.min(clock.getElapsedTime()/1.0, 1.0) * roomRotation;
+    if(room && roomRotation > 0){
+        let dy = dt * 90;
+        room.rotation.y += THREE.MathUtils.degToRad(dy) * rotateDir;
+        roomRotation -= dy;
     }
     renderer.render( scene, camera );
 })();
 
 //===============================================================================================//
 
-function rotate(){
-    const cube = document.getElementsByClassName('cube')[0];
-    cube.classList.add('rotated');
-    roomRotation = THREE.MathUtils.degToRad(90);
-    clock.start();
+function rotate(w){
+    let c;
+    let deg = 0;
+    switch(wall){
+        case 0: switch(w){
+            case 1: deg = 90; c = 90; break;
+            case 3: deg = -90; c = -90; break;
+        } 
+        case 1: switch(w){
+            case 0: deg = 90; c = 0; break;
+            case 2: deg = -90; c = 180; break;
+        } 
+        case 2: switch(w){
+            case 1: deg = 90; c = 90; break;
+            case 3: deg = -90; c = 270; break;
+        } 
+        case 3: switch(w){
+            case 2: deg = 90; c = 180; break;
+            case 0: deg = -90; c = 0; break;
+        } 
+    }
+    wall = w;
+    roomRotation = (deg == 0 ? 0 : 90);
+    rotateDir = Math.sign(deg);
+    console.log(deg);
+    const cube = document.getElementById('cube');
+    cube.setAttribute("class" , `rotate${c}`);
 }
 
-document.getElementsByClassName("cube__face--back")[0].addEventListener("click", rotate);
+document.getElementsByClassName("cube__face--left")[0].addEventListener("click", () => rotate(1));
+document.getElementsByClassName("cube__face--back")[0].addEventListener("click", () => rotate(0));
+document.getElementsByClassName("cube__face--right")[0].addEventListener("click", () => rotate(3));
+document.getElementsByClassName("cube__face--front")[0].addEventListener("click", () => rotate(2));
